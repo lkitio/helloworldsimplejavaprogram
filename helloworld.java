c9abaaeb7471dd7ea4f8537f4d0b9448a9b136b9
lkitio <135192120+lkitio@users.noreply.github.com>
2023-06-12 21:19:02 -0400

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello batch 30 team with lionel and team");
+      System.out.println("Hello batch 30 team with students RUNNING JENKINS SUCESSFULLY ");
+    }  
+} 
